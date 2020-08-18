51bbe20c22f23d5a3aa80aa4b79315f2f740f95d
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-18 21:33:01 +0530

    Finalized Code with capability to handle spaces also!

--- Ceaser-Cipher/C++/ceaser.cpp
+++ Ceaser-Cipher/C++/ceaser.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
 
 string encrypt(string p, int n){
     string s = "";
     for (int i = 0; i < p.length(); i++){
-        if(isupper(p[i])){
+        if(p[i] == ' '){
+           s += p[i]; 
+        }
+        else if(isupper(p[i])){
             s += (((p[i] + n) - 65) % 26) + 65;
         }
         else
         {
             s += (((p[i] + n) - 97) % 26) + 97;
         }
-        
     }
     return s;
 }
 
 int main(){
-    string str;
-    cin>>str;
+    string str; int key;
 
+    cout<<"Enter the Text to be Encrypted!"<<endl;
+    getline(cin, str);
+    cout<<"Enter the Key for the Encryption!"<<endl;
+    cin>>key;
     string s1;
-    s1 = encrypt(str, 5);
-    cout<<"Encrpyted Text: "<<s1<<endl<<"Decrypted Text: "<<encrypt(encrypt(str, 5), 21)<<endl;
+    //s1 = encrypt(str, key);
+
+    cout<<"Encrpyted Text: "<<encrypt(str, key)<<endl<<"Decrypted Text: "<<encrypt(encrypt(str, key), (26 - key))<<endl;
 
     return 0;
 }
