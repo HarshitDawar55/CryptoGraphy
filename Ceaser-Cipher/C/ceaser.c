96ab2f5e0653ecfb3b1101032ac85b5cc78e9bb7
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-18 20:59:06 +0530

    Had some issue in this code for proper functioning!

--- Ceaser-Cipher/C/ceaser.c
+++ Ceaser-Cipher/C/ceaser.c
@@ -1,34 +1,46 @@
 #include<stdio.h>
 #include<ctype.h>
 
-char *encrypt(char p[], int n){
+void encrypt(char p[], int n){
     char *s;
     for (int i = 0; p[i] != '\0'; i++){
         if(isupper(p[i])){
-            s[i] += (((p[i] + n) - 65) % 26) + 65;
+            s[i] += ((*(p + i) + n - 65) % 26) + 65;
         }
         else
         {
-            s[i] += (((p[i] + n) - 97) % 26) + 97;
+            s[i] += ((*(p + i) + n - 97) % 26) + 97;
         }        
     }
-    return s;
+    s += '\0';
+    printf("%s\n", s);
 
+    char *decrypted_string;
+
+    for(int j = 0; s[j]!='\0'; j++){
+        if(isupper(s[j])){
+           decrypted_string[j] += ((*(s + j) - n - 65) % 26) + 65; 
+        }
+        else
+        {
+            decrypted_string[j] += ((*(s + j) - n - 97) % 26) + 97; 
+        }
+    }
+    decrypted_string += '\0';
+    printf("Decrypted String: %s\n", decrypted_string);
 }
 
+
 int main(){
-    char *str; int key;
+    char *str;
+    int key;
     printf("Enter the string!\n");
-    scanf("%s", str);
+    gets(str);
     printf("Enter the key for Encryption!\n");
     scanf("%d", &key);
 
     // Printing the Encrypted String
     printf("Encrypted String is: \n");
-    printf("%s\n", encrypt(str, key));
-
-    // Printing the Decrypted String
-    //printf("Decrypted String is: \n");
-    //encrypt(encrypt(str), 26 - key);
+    encrypt(str, key);
     return 0;
 }
