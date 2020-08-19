7af71fd76ce8bdd5003505e98c6dc510db11602c
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-19 20:42:41 +0530

    Finalized Code for the Ceaser-Cipher

--- src/ceaser_cipher.java
+++ src/ceaser_cipher.java
@@ -1,6 +1,60 @@
+import java.util.Scanner;
 
-public class ceaser_cipher {
+class encrypt_decrypt{
+    // Creating a function to encrypt the Text
+    static String encrypt(String Text, int key){
+        String encrypted_Text = "";
+        char []temp = Text.toCharArray();
+        for(char character : temp){
+            if(Character.isSpaceChar(character)){
+                encrypted_Text += (char)character;
+            }
+            else if(Character.isUpperCase(character)){
+                encrypted_Text += (char)((((int)character + key - 65) % 26) + 65);
+            }
+            else {
+                encrypted_Text += (char)((((int)character + key - 97) % 26) + 97);
+            }
+        }
+        return encrypted_Text;
+    }
+
+    // Creating a function to decrypt the Text
+    static String decrypt(String Text, int key){
+        String decrypted_Text = "";
+        char []temp = Text.toCharArray();
+        for(char character : temp){
+            if(Character.isSpaceChar(character)){
+                decrypted_Text += (char)character;
+            }
+            else if(Character.isUpperCase(character)){
+                decrypted_Text += (char)((((int)character - key - 65) % 26) + 65);
+            }
+            else {
+                decrypted_Text += (char)((((int)character - key - 97) % 26) + 97);
+            }
+        }
+        return decrypted_Text;
+    }
+}
+
+public class ceaser_cipher extends encrypt_decrypt{
     public static void main(String[] args) {
+        System.out.println("Enter the Text to be Encrypted!");
+        String Text, encrypted_Text, decrypted_Text; int key;
+        Scanner take_input = new Scanner(System.in);
+
+        Text = take_input.nextLine();
+
+        System.out.println("Enter the key for the Encryption!");
+
+        key = take_input.nextInt();
+
+        encrypted_Text = encrypt(Text, key);
+        decrypted_Text = decrypt(encrypted_Text, key);
+
+        System.out.println(encrypted_Text);
+        System.out.println(decrypted_Text);
 
     }
 }
