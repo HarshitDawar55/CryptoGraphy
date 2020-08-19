c22ffa76f67b2f71824fe9e975ee2e10ff2f7c6d
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-19 21:20:03 +0530

    Finalized code to encrypt & Decrypt with StringBuilder!

--- src/ceaser_cipher.java
+++ src/ceaser_cipher.java
@@ -18,30 +18,12 @@ class encrypt_decrypt{
         }
         return encrypted_Text.toString();
     }
-
-    // Creating a function to decrypt the Text
-    static String decrypt(String Text, int key){
-        StringBuilder decrypted_Text = new StringBuilder();
-        char []temp = Text.toCharArray();
-        for(char character : temp){
-            if(Character.isSpaceChar(character)){
-                decrypted_Text.append(character);
-            }
-            else if(Character.isUpperCase(character)){
-                decrypted_Text.append((char) ((((int) character - key - 65) % 26) + 65));
-            }
-            else {
-                decrypted_Text.append((char) ((((int) character - key - 97) % 26) + 97));
-            }
-        }
-        return decrypted_Text.toString();
-    }
 }
 
 public class ceaser_cipher extends encrypt_decrypt{
     public static void main(String[] args) {
         System.out.println("Enter the Text to be Encrypted!");
-        String Text, encrypted_Text, decrypted_Text; int key;
+        String Text, encrypted_Text; int key;
         Scanner take_input = new Scanner(System.in);
 
         Text = take_input.nextLine();
@@ -51,10 +33,11 @@ public class ceaser_cipher extends encrypt_decrypt{
         key = take_input.nextInt();
 
         encrypted_Text = encrypt(Text, key);
-        decrypted_Text = decrypt(encrypted_Text, key);
 
+        System.out.println("Encrypted Text!");
         System.out.println(encrypted_Text);
-        System.out.println(decrypted_Text);
+        System.out.println("Decrypted Text!");
+        System.out.println(encrypt(encrypted_Text, 26 - key));
 
     }
 }
