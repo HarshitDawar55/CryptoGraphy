fd09b3e60d4c36079fcc935b9e3bfca34c658df4
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-21 19:11:20 +0530

    Efficient Code to calculate the GCD of 2 Numebrs!

--- GCD/gcd.py
+++ GCD/gcd.py
@@ -1,9 +1,12 @@
+# Best function to calculate the GCD using the remainder concept!
 def gcd(a, b):
     if a == 0:
         return b
     else:
         return gcd(b % a, a)
 
+# Advanced Input Taking Statement Integrating the concepts of Integer Mapping & List Unpacking!
 n1, n2 = list(map(int, input("Enter 2 Numbers\n").split()))
 
+# Statement to Print the GCD/HCF of the 2 numbers!
 print(gcd(n1, n2))
