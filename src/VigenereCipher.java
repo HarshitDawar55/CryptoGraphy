71d85e2ffbefbb3e3a4031c4aad5a051489b8dde
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-22 10:24:19 +0530

    Started creating code from main function

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class VigenereCipher {
+    public static void main(String[] args) {
+        // Declaring Scanner for the Input
+        Scanner sc = new Scanner(System.in);
+        String Text, CipherText, Key;
+
+        System.out.println("Enter the Text to be Encrypted");
+        Text = sc.nextLine();
+
+        System.out.println("Enter the Key for the Encryption");
+        Key = sc.nextLine();
+    }
 }
