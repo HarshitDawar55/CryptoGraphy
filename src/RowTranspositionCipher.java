2e058ced2495fa8074d3a25e62c0f3a5b40af814
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-22 17:23:10 +0530

    Dummy charaters are clculated successfully

--- src/RowTranspositionCipher.java
+++ src/RowTranspositionCipher.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class RowTranspositionCipher {
@@ -22,19 +23,48 @@ public class RowTranspositionCipher {
         return numericKey;
     }
 
-    static void calculateMatrixDimensions(String Text, String Key){
-        int numberOfRows = Text.length() / Key.length();
-        int numberOfColumns = Key.length();
-        
+    static String calculateDummyCharacters(String Text, int []Key, int numberOfRows, int numberOfColumns){
+        int numberOfDummyCharacters = Key.length - (numberOfRows % numberOfColumns);
+        StringBuilder newText = new StringBuilder(Text);
+        // Calculating there is a need of dummy characters
+        if ( numberOfDummyCharacters != 0){
+            String []dummyCharacters = {"X", "Y", "Z"};
+
+            Random rand = new Random();
+            for(int i = 0; i < numberOfDummyCharacters; i++){
+                newText.append(
+                        dummyCharacters[
+                                rand.nextInt(dummyCharacters.length)
+                                ]
+                );
+            }
+        }
+        return newText.toString();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String Key;
+        String Key, Text;
         int []numKey;
+        int numberOfRows, numberOfColumns;
+
+        System.out.println("Enter the Text");
+        Text = sc.nextLine();
+
         System.out.println("Enter the key");
         Key = sc.nextLine();
+
         numKey = keyGenerator(Key);
         System.out.println(Arrays.toString(numKey));
+
+        numberOfRows = Text.length() / numKey.length;
+        numberOfColumns = numKey.length;
+
+        if(Text.length() % numKey.length != 0 ){
+            Text = calculateDummyCharacters(Text, numKey, numberOfRows, numberOfColumns);
+        }
+
+        System.out.println(Text);
+        System.out.println(Arrays.toString("HARSHI DAWAR".split(" ")));
     }
 }
