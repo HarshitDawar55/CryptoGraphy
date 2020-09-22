8f0b1f8e50d15861a3a2ef3e2a56af6dc3e267a6
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-22 16:11:50 +0530

    Finalized the File

--- src/UserAuthentication.java
+++ src/UserAuthentication.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class UserAuthentication {
+    public static void main(String[] args) {
+        String username, password;
+        Scanner sc = new Scanner((System.in));
+
+        System.out.println("Enter the Username!");
+        username = sc.nextLine();
+
+        System.out.println("Enter the Password!");
+        password = sc.nextLine();
+
+        System.out.println("Username Entered: " + username);
+        System.out.print("Password Entered: ");
+        for(int i = 0; i < password.length(); i++){
+            System.out.print("*");
+        }
+    }
 }
