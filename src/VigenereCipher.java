74cfe4fc7599dbf516594788d5d5ab03978036b0
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-22 10:45:48 +0530

    Key Generation Function Complete

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -1,6 +1,35 @@
 import java.util.Scanner;
 
+
 public class VigenereCipher {
+
+    static String generateKey(String Text, String Key){
+        String[] temp;
+        temp = Text.split(" ");
+        StringBuilder tempBuilder = new StringBuilder();
+
+        // Constructing String back after removing the spaces
+        for(String word : temp){
+            tempBuilder.append(word);
+        }
+
+        Text = tempBuilder.toString();
+
+        int strLen = Text.length(), keyLen = Key.length();
+        if(strLen == keyLen){
+            return Key;
+        }
+        int i = 0;
+        StringBuilder KeyBuilder = new StringBuilder(Key);
+        while (keyLen < strLen){
+            KeyBuilder.append(KeyBuilder.charAt(i));
+            keyLen += 1;
+            i += 1;
+        }
+        Key = KeyBuilder.toString();
+        return Key;
+    }
+
     public static void main(String[] args) {
         // Declaring Scanner for the Input
         Scanner sc = new Scanner(System.in);
@@ -11,5 +40,8 @@ public class VigenereCipher {
 
         System.out.println("Enter the Key for the Encryption");
         Key = sc.nextLine();
+
+        Key = generateKey(Text, Key);
+        System.out.println(Key);
     }
 }
