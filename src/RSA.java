a402a97ea2ab6490d5531ba99c170c2df43e30aa
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-10-25 17:55:31 +0530

    PrimeCheck Implemented

--- src/RSA.java
+++ src/RSA.java
@@ -1,4 +1,19 @@
+
 public class RSA {
+    public static boolean PrimeCheck(int n){
+        if(n < 2){
+            return false;
+        }
+        for(int i = 2; i < Math.sqrt(n) + 1; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+
     public static void main(String[] args) {
 
     }
