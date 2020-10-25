a0d03563918d40a9eae2f0bc19c33670c5639c78
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-10-25 21:30:33 +0530

    e calculated!

--- src/RSA.java
+++ src/RSA.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 public class RSA {
     // Function to check whether a given number is prime or not
@@ -26,6 +30,30 @@ public class RSA {
     // Function to calculate Euler Totient Function.
     public static int cal_Euler_Totient_Function(int p, int q){
         return (p - 1) * (q - 1);
+    }
+    public static int[] Key_Generator(){
+        Scanner sc = new Scanner(System.in);
+        ArrayList Elements = new ArrayList();
+
+        System.out.println("Enter the 2 Prime Numbers");
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+
+        if (!PrimeCheck(p) || !PrimeCheck(q)){
+            exit(0);
+        }
+
+        int n = p * q;
+        int phi = cal_Euler_Totient_Function(p, q);
+        int e = 2, d = 1;
+
+        while (!(gcd(e, phi) == 1) && e < phi){
+            e += 1;
+        }
+
+        System.out.println("e: " + e);
+
+        
     }
 
     public static void main(String[] args) {
