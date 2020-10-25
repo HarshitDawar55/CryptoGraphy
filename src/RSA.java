fc7a9cf708255004ff511a8b0cc99ed3dbd8fd6e
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-10-25 17:59:08 +0530

    GCD & euler Totient Implemented

--- src/RSA.java
+++ src/RSA.java
@@ -1,5 +1,6 @@
 
 public class RSA {
+    // Function to check whether a given number is prime or not
     public static boolean PrimeCheck(int n){
         if(n < 2){
             return false;
@@ -12,7 +13,20 @@ public class RSA {
         return true;
     }
 
-    
+    // Function to calculate gcd of 2 numbers in the best way.
+    public static int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        else{
+            return gcd(b % a, a);
+        }
+    }
+
+    // Function to calculate Euler Totient Function.
+    public static int cal_Euler_Totient_Function(int p, int q){
+        return (p - 1) * (q - 1);
+    }
 
     public static void main(String[] args) {
 
