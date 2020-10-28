bbb5b6151ccd9c9b6e257ce9ef6e0bb58ea33cd3
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-10-28 18:09:08 +0530

    KeyGenerator Implemented

--- RSA/C++/RSA.cpp
+++ RSA/C++/RSA.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+#include<vector>
 using namespace std;
 
 bool primeCheck(int n){
@@ -25,11 +26,47 @@ int cal_Euler_Totient_Function(int p, int q){
 	return (p - 1) * (q - 1);
 }
 
+int KeyGenerator(){
+	std::vector<int> vec;
+	int p, q;
 
+	cout<<"Enter two prime numbers!"<<endl;
+	cin>>p>>q;
+
+	if(!primeCheck(p) || !primeCheck(q)){
+		exit(0);
+	}
+
+	int n = p * q;
+	int phi = cal_Euler_Totient_Function(p, q);
+
+	int k = 2, v = 1;
+
+	while (!(gcd(k, phi) == 1)){
+
+		k += 1;
+	}
+
+	cout<<"k: "<<k<<endl;
+
+	while (!((k * v) % phi == 1)){
+		v += 1;
+	}
+
+	vec[0] = k;
+	vec[1] = v;
+	vec[2] = n;
+
+	return v;
+
+}
 
 int main(){
 
-	cout<<primeCheck(5);
+	int plainText, cipherText, decryptedText;
+
+	cout<<"Enter Plain Text to be encrypted!"<<endl;
+	cin>> plainText; 
 	return 0;
 }
 
