ef6fca446065aecc68b8f3504af255fefa8dfff7
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-06 13:36:34 +0530

    Started Implementing the Key Generator

---
+++ src/SDES.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class SDES {
+    public static ArrayList<Integer> P10(ArrayList<Integer> Key){
+        ArrayList<Integer> P10Key = new ArrayList<Integer>();
+        P10Key.add(Key.get(2));
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
