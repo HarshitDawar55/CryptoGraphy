17aa37bbc7c3e9decbbd0219abed256b34e7bb72
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-06 18:26:21 +0530

    P8 Implemented!

--- src/SDES.java
+++ src/SDES.java
@@ -46,19 +46,49 @@ public class SDES {
         return ShiftedKeys;
     }
 
+    public static List<Integer> P8(List<Integer> Key){
+        List<Integer> P8Key = new ArrayList<Integer>();
+
+        // Changing the Key to the specified numbering of P10 block that is [6, 3, 7, 4, 8, 5, 10, 9]
+        P8Key.add(Key.get(5));
+        P8Key.add(Key.get(2));
+        P8Key.add(Key.get(6));
+        P8Key.add(Key.get(3));
+        P8Key.add(Key.get(7));
+        P8Key.add(Key.get(4));
+        P8Key.add(Key.get(9));
+        P8Key.add(Key.get(8));
+
+        return P8Key;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> Key = new ArrayList<Integer>();
         List<Integer> P10K;
+        List<Integer> LeftHalf, RightHalf;
 
+        // Taking Key as Input from the User!
         System.out.println("Enter the Key for Encryption!");
         while(sc.hasNextInt()){
             Key.add(sc.nextInt());
         }
 
+        // Generating P10 key
         System.out.println(Key);
         P10K = P10(Key);
+        System.out.println("P10 Key: " + P10K);
+
+        // Generating Left & Half Key for First Round
+        LeftHalf = LeftShift(P10K).get("LeftHalf");
+        RightHalf = LeftShift(P10K).get("RightHalf");
+        System.out.println("LH: " + LeftHalf + " " + "RH: " + RightHalf);
+
+        // Assigning Transformed Key Parts to the Original Key
+        P10K.clear();
         System.out.println(P10K);
-        System.out.println(LeftShift(P10K));
+        P10K.addAll(LeftHalf);
+        P10K.addAll(RightHalf);
+        System.out.println(P10K);
     }
 }
