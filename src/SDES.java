f7f5977a9bc7743a07c3c886ae192470733b604d
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-06 13:38:33 +0530

    P10 key implemented!

--- src/SDES.java
+++ src/SDES.java
@@ -3,7 +3,20 @@ import java.util.ArrayList;
 public class SDES {
     public static ArrayList<Integer> P10(ArrayList<Integer> Key){
         ArrayList<Integer> P10Key = new ArrayList<Integer>();
+
+        // Changing the Key to the specified numbering of P10 block that is [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
         P10Key.add(Key.get(2));
+        P10Key.add(Key.get(4));
+        P10Key.add(Key.get(1));
+        P10Key.add(Key.get(6));
+        P10Key.add(Key.get(3));
+        P10Key.add(Key.get(9));
+        P10Key.add(Key.get(0));
+        P10Key.add(Key.get(8));
+        P10Key.add(Key.get(7));
+        P10Key.add(Key.get(5));
+
+        return P10Key;
     }
 
 
