59bfeaa5c42bc1c6e52d0254e9cf65388065bbc4
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-06 13:54:16 +0530

    Left Shift Function implemented!

--- src/SDES.java
+++ src/SDES.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SDES {
-    public static ArrayList<Integer> P10(ArrayList<Integer> Key){
-        ArrayList<Integer> P10Key = new ArrayList<Integer>();
+    public static List<Integer> P10(ArrayList<Integer> Key){
+        List<Integer> P10Key = new ArrayList<Integer>();
 
         // Changing the Key to the specified numbering of P10 block that is [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
         P10Key.add(Key.get(2));
@@ -19,6 +22,30 @@ public class SDES {
         return P10Key;
     }
 
+    public static Map<String, List<Integer>> LeftShift(List<Integer> Key){
+        // Declaring two Array Lists for left half & right half of the original Key
+        List<Integer> LeftHalf = new ArrayList<Integer>();
+        List<Integer> RightHalf = new ArrayList<Integer>();
+
+        // Appending into Left Half & Right Half
+        LeftHalf = Key.subList(0, Key.size() / 2);
+        RightHalf = Key.subList(Key.size() / 2, Key.size());
+
+        // Performing Left Shift
+        int temp = LeftHalf.remove(0);
+        LeftHalf.add(temp);
+
+        temp = RightHalf.remove(0);
+        RightHalf.add(temp);
+
+        // Initializing the HashMap for the lists
+        Map<String, List<Integer>> ShiftedKeys = new HashMap<String, List<Integer>>();
+        ShiftedKeys.put("LeftHalf", LeftHalf);
+        ShiftedKeys.put("RightHalf", RightHalf);
+
+        // Returning the Shifted Keys
+        return ShiftedKeys;
+    }
 
     public static void main(String[] args) {
 
