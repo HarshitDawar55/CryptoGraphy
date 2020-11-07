076b212ed04a6998f38777cbe26500a4363402fe
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-07 11:07:08 +0530

    W2 half Key done!

--- src/S_AES.java
+++ src/S_AES.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class S_AES {
     public static List<Integer> ConcatenateSubKeys(List<Integer> SubKey1, List<Integer> SubKey2){
@@ -10,6 +8,25 @@ public class S_AES {
         MergedKey.addAll(SubKey2);
 
         return MergedKey;
+    }
+
+    public static Map<String, List<Integer>> GenComplexKeys(List<Integer> SubKey1, List<Integer> SubKey2, List<Integer> XORList){
+        Map<String, List<Integer>> ComplexKeys = new HashMap<>();
+        List<Integer> FirstHalf = new ArrayList<>(), SecondHalf = new ArrayList<>(), temp = new ArrayList<>();
+
+        // Generating First XOR Part of the FirstHalf of the Complete FirstHalf Key.
+        for(int i = 0; i < 8; i++){
+            FirstHalf.add(SubKey1.get(i) ^ XORList.get(i));
+        }
+
+        // Rotating Nibbles of the SubKey2
+        for(int i = 0; i < 4; i++){
+            temp.add(SubKey2.remove(i));
+        }
+        SubKey2.addAll(temp);
+        temp.clear();
+
+
     }
     public static void main(String[] args) {
         String tempkey, tempText;
